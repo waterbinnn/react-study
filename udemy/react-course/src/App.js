8d1021bfec9e14ffa9eee24aafb2f61ec5335a65
Subin <ewaterbin@naver.com>
2022-06-08 22:27:01 +0900

    리액트 todo만들기 시작

---
+++ udemy/react-course/src/App.js
@@ -0,0 +1,14 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="Learn React" />
+      <Todo text="Eat Hamberger" />
+      <Todo text="Learn Resume" />
+    </div>
+  );
+}
+
+export default App;
---
+++ udemy/react-course/src/components/Todo.js
@@ -0,0 +1,14 @@
+function Todo(props) {
+  return (
+    <div>
+      <div className="card">
+        <h2>{props.text}</h2>
+        <div className="actions">
+          <button className="btn">Delete</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Todo;
