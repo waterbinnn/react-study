a2fba1e8d9a3ec0f206ef003f277f314f9fe298e
waterbinnn <ewaterbin@naver.com>
2022-06-23 10:13:27 +0900

    미니블로그 만들기

---
+++ mini-blog/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
+import "./App.css";
+//pages
+import MainPage from "./component/page/MainPage";
+import PostWritePage from "./component/page/PostWritePage";
+import PostViewPage from "./component/page/PostViewPage";
+
+const MainTitleText = styled.p`
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+`;
+
+function App(props) {
+  return (
+    <BrowserRouter>
+      <MainTitleText>Mini Blog</MainTitleText>
+      <Routes>
+        <Route index element={<MainPage />} />
+        <Route path="post-write" element={<PostWritePage />} />
+        <Route path="post/:postId" element={<PostViewPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
